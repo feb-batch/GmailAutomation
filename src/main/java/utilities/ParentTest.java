273ce3dc085dc5a9390bff52e1d146b2cbd852dd
John Doe <john@sample.com>
2018-06-03 16:20:22 +0530

    <Niraj> Changes made on 03th June 2016

--- src/main/java/utilities/ParentTest.java
+++ src/main/java/utilities/ParentTest.java
@@ -6,39 +6,78 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Properties;
 
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeSuite;
 
 public class ParentTest {
-	
-	protected ChromeDriver driver = null;
+
+	protected WebDriver driver = null;
 	String testName = this.getClass().getSimpleName();
-	Properties properties = null;	
-	// to read test data
+	Properties dataProperties = null;
+	static Properties configProperties = null;
+
+	static {
+		System.out.println("in static block");
+		configProperties = new Properties();
+		try {
+			configProperties.load(new FileInputStream(new File(System.getProperty("user.dir")+"\\src\\test\\Config\\CommonConfigs.properties")));
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
 	
 	// to launch browser
 	@BeforeClass
 	public void setUp() {
-		System.setProperty("webdriver.chrome.driver", "C:\\Users\\vshar10\\Downloads\\driver\\chromedriver.exe");
-		driver = new ChromeDriver();
-		driver.get("http://gmail.com");
+		// Get browser type
+		String browserType = getConfig("browserType");
+		switch (browserType) {
+		case "FF":
+			System.setProperty("webdriver.gecko.driver", System.getProperty("user.dir")+"\\src\\test\\Drivers\\geckodriver.exe");
+			driver = new FirefoxDriver();
+			break;
+		case "CHROME":
+			System.out.println("in chrome");
+			System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"\\src\\test\\Drivers\\chromedriver.exe");
+			driver = new ChromeDriver();
+			break;
+		case "IE":
+			break;
+		default:
+			System.out.println("Incorrect browser selected.");
+			System.exit(0);
+		}
+		
+		driver.get(getConfig("baseUrl"));
 	}
-	
-	
+
+	public String getConfig(String key) {
+		return configProperties.getProperty(key);
+	}
+
+
 	// to quit browser
 	@AfterClass
 	public void tearDown() {
 		driver.quit();
 	}
-	
+
 	public String getData(String key) {
 		// Get class name
-		if (properties == null) {
+		if (dataProperties == null) {
 			String filePath = System.getProperty("user.dir")+"\\src\\test\\DataFiles\\"+testName+".properties";
-			properties = new Properties();
+			dataProperties = new Properties();
 			try {
-				properties.load(new FileInputStream(new File(filePath)));
+				dataProperties.load(new FileInputStream(new File(filePath)));
 			} catch (FileNotFoundException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
@@ -47,8 +86,8 @@ public class ParentTest {
 				e.printStackTrace();
 			}
 		}
-		return properties.getProperty(key);
+		return dataProperties.getProperty(key);
 	}
-	
-	
+
+
 }
